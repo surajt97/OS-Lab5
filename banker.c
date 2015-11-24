e99e031bc6edcd0d3be040fdafdb1d41cdfac8e0
jrunzer26 <jason.runzer@uoit.net>
2015-11-24 17:11:47 -0500

    ready to submit

--- banker.c
+++ banker.c
@@ -8,7 +8,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
- #include <time.h> //Used for random function
+#include <time.h> //Used for random function
 #include <stdbool.h>
 #include <pthread.h>
 #include <semaphore.h>
@@ -38,98 +38,66 @@ pthread_mutex_t lock;
 
 
 
-// Define functions declared in banker.h here
+/*
+* The method run by each customer, to request and release resources.
+* The customer will request a random numnber of resources and if it is granted,
+* the customer will sleep for up to 5 seconds. Then the customer will release a different
+*  random number of reasoruces even if it was not granted resources to use. This will only
+*  release the amount that the resource has.
+* @param - customerNum - the number of the customer in the system
+*/
 void *customerMethod(void *customerNum){
+    //cast the customer number
     int customer = *(int*)customerNum;
     bool acquiredResources = false;
+
+    //the arrays of the resources for the specific customer
     int request [NUM_RESOURCES];
     int release [NUM_RESOURCES];
     
     while(1){
         for(int i = 0; i < NUM_RESOURCES; i++){
-            request[i] = rand() % (maximum[customer][i] + 1);
+            //request a random number of resources from 0 to the maximum in the system
+            //this will allow the needy state to become true sometimes
+            request[i] = rand() % (maximum[customer][i] + 1); 
         }
         pthread_mutex_lock(&lock);
+        //try to get the resources
         acquiredResources = request_res(customer,request);
         pthread_mutex_unlock(&lock);
         if(acquiredResources){
-            printf("Customer: %d got the resources it wanted! The customer says they will use them and give them back in a bit.\n\n", customer);
+            printf("This is a safe state\nCustomer: %d got the resources it wanted! The customer will return some in at most 5 seconds.\n\n", customer);
             sleep((int)rand() % 5 + 1);
-            
             acquiredResources = false;
         }
+        //generate resources to release
         for(int i = 0; i < NUM_RESOURCES; i++){
             srand(time(NULL));
             release[i] = rand() % (allocation[customer][i] + 1);
         }
         pthread_mutex_lock(&lock);
-        release_res(customer,release);
+        release_res(customer,release); //release the resources
         pthread_mutex_unlock(&lock);
     }
 
 }
 
-bool is_safe() {
-    //Create & initialize variables for checking the safe sequences
-    int size = sizeof(available) / sizeof(available[0]);
-    int work[size];
-    bool finish[NUM_CUSTOMERS];
-    int finishCounter=0;
-    bool safe = true;
 
-
-    //Copy the request array to the work array.
-    for(int i = 0; i <= size; i++){
-        work[i] = available[i];
-        
-    }
-
-    //Initialize finish array
-    for (int j = 0; j<NUM_CUSTOMERS; j++) {
-        finish[j] = false;
-    }
-
-    /* Nested for loop for traversing the 2 dimensional arrays
-    *if a given need element is less or equal to a corresponding work element, and if the current finishCounter is false, we want to increment the work value by the corresponding allocation element
-    *Otherwise, we want to keep our given finish element at false and increment the counter
-    *The final finish array will will determine the outcome of is_safe
-    */
-    for (int row = 0; row < NUM_RESOURCES; row++) {
-        for (int column = 0; column < NUM_CUSTOMERS; column++) {
-            if ((need[row][column] <= work[column]) && (finish[finishCounter] == false)) {
-                work[column] = work[column] + allocation[row][column];
-                finish[finishCounter] = true;
-                finishCounter++;
-            } else {
-                finish[finishCounter] = false;
-                finishCounter++;
-                
-            }
-
-        }
-    }
-
-
-    for (int count2=0; count2<NUM_CUSTOMERS; count2++) {
-        if (finish[count2] != true) {
-            safe = false;
-            break;
-        }
-    }
-    return safe;
-
-   
-}
-
+/*
+* Requests and alloctes resources for a customer if possible.
+* Returns false if it is not possbile to allocate the resources for the customer
+* @param - n_customer - the customer number
+* @param request - the array of resources the customer wishes to recieve
+*/
 bool request_res(int n_customer, int request[]){
    
     printf("Customer: %d Requesting Resources\n",n_customer);
     for(int i = 0; i < NUM_RESOURCES; i++){
         printf("%d ",request[i]);
     }
-
     puts("");
-     printf("Available Resources Before\n");
+
+    printf("Available Resources Before\n");
     for(int i = 0; i < NUM_RESOURCES; i++){
         printf("%d ",available[i]);
     }
@@ -137,13 +105,13 @@ bool request_res(int n_customer, int request[]){
      for(int i = 0; i < NUM_RESOURCES; i++){
         if(request[i] <= need[n_customer][i]){
             if(request[i] > available[i]){
-                printf("THIS WOULD LEAD TO AN UNSAFE STATE! ABORT ABORT! ALL HANDS ON DECK! FUCK WE GONNA DIE!!!!\n");
-                sleep(2);
+                printf("THIS WOULD LEAD TO AN UNSAFE STATE! THE SYSTEM CANNOT ALLOCATE THE REQUESTED RESOURCES\n");
+                sleep(2); //used for readability
                 return false;
             }
         } else{
-            printf("Customer: %d is WAY TOO NEEDY\n",n_customer);
-            sleep(2);
+            printf("Customer: %d is NEEDY\n",n_customer);
+            sleep(2);//used for redability
             return false;
         }
 
@@ -161,46 +129,20 @@ bool request_res(int n_customer, int request[]){
     puts("");
     return true;
 }
-/*
- bool request_res(int n_customer, int request[])
-{
 
-    for (int j = 0; j<NUM_RESOURCES; j++) {
-        if (request[j] <= need[n_customer][j]) {
-            if (request[j] <= available[j]) {
-                printf("Request valid so far");
-                available[j] = available[j] - request[j];
-                allocation[n_customer][j] = allocation[n_customer][j] + request[j];
-                need[n_customer][j] = need[n_customer] - request[j];
 
-                if (is_safe()) {
-                    //Do nothing; allocation is safe
-                } else {
-                    //Return resources
-                }
-
-            } else {
-                //Wait; resources unavailable
-            }
-        } else {
-            //raise error condition, exceeded max amount
-        }
-    }
-
-    return true;
-}
-
+/*Release resources, returns true if successful
+* @param - n_customer - the customer number wishing to release resources
+* @param - release - the array of respources to release
 */
-// Release resources, returns true if successful
 bool release_res(int n_customer, int release[])
-{   
-    puts("");
-    printf("Customer: %d Releasing Resources\n", n_customer);
+{  
+    printf("\nCustomer: %d Releasing Resources\n", n_customer);
     for(int i = 0; i < NUM_RESOURCES; i++){
         printf("%d ",release[i]);
-        available[i] += release[i];
-        need[n_customer][i] += release[i];
-        allocation[n_customer][i] -= release[i];
+        available[i] += release[i]; //add to the available resources
+        need[n_customer][i] += release[i]; //add to the customers need array
+        allocation[n_customer][i] -= release[i]; //reduce the allocation for the specific customer
     }
     puts("");
     printf("Available Resources\n");
@@ -215,8 +157,6 @@ bool release_res(int n_customer, int release[])
 
 int main(int argc, char *argv[])
 {   
-    
-    // ==================== YOUR CODE HERE ==================== //
 
     // Read in arguments from CLI, NUM_RESOURCES is the number of arguments   
     pthread_t threads[NUM_CUSTOMERS];
@@ -225,16 +165,14 @@ int main(int argc, char *argv[])
         //Specifies the max INITIAL availale resources. MAX cannot exceed this
         available[i] = atoi(argv[i+1]);
         
+        //go through the customers and randomly state how many resources they need at maximum
         for(int j=0; j<NUM_CUSTOMERS; j++) {
-            //srand(time(NULL));
-            
             maximum[j][i] = rand() % (available[i] + 1);
-            //allocation[j][i] = rand()% available[i];
-            //printf("%d ", maximum[j][i]);
             need[j][i] = maximum[j][i];
         }        
     }
 
+    //print out the needs of all customers
     printf("Maximum Needs\n");
     for(int i = 0; i < NUM_CUSTOMERS; i++){
         for(int j = 0; j < NUM_RESOURCES; j++){
@@ -242,47 +180,26 @@ int main(int argc, char *argv[])
         }
         puts("");
     }
-
+    srand(time(NULL));
+    //make all the customers
     for(int i = 0; i < NUM_CUSTOMERS; i++){
         int *c_num = malloc(sizeof(*c_num));
+        //make an integer pointer to be used to send into the customer thread
         if(c_num ==NULL){
             printf("couldn't make customer number");
             exit(1);
         }
         *c_num = i;
+        //create the customer thread
         pthread_create(&threads[i],NULL,&customerMethod, c_num);
     }
     
-    //join all the threads
+    //join all the threads (will never happen)
     for(int i = 0; i < NUM_CUSTOMERS; i++){
         pthread_join(threads[i],0);
     }
+
     pthread_mutex_destroy(&lock);
-    //int max_resources [NUM_RESOURCES];
-
-    printf("Made to check safe\n");
     
-    if(is_safe()) {
-        printf("System is safe\n");
-    } else {
-        printf("System is not safe\n");
-    }
-    
-    // Allocate the available resources
-    //if request res is true, allocate
-
-
-    // Initialize the pthreads, locks, mutexes, etc.
-
-    // Run the threads and continually loop
-
-    // The threads will request and then release random numbers of resources
-
-    // If your program hangs you may have a deadlock, otherwise you *may* have
-    // implemented the banker's algorithm correctly
-    
-    // If you are having issues try and limit the number of threads (NUM_CUSTOMERS)
-    // to just 2 and focus on getting the multithreading working for just two threads
-
     return EXIT_SUCCESS;
 }
